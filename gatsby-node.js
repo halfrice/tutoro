2c77be2ac472257125343155a9c7fc619d2b7932
halfrice <halfrice@pm.me>
2020-02-28 00:35:46 -0800

    Add site config

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,7 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     resolve: {
       alias: {
         "~components": path.resolve(__dirname, "src/components"),
+        "~config": path.resolve(__dirname, "src/config"),
         "~styles": path.resolve(__dirname, "src/styles"),
       },
     },
---
+++ src/config/index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  site: {
+    title: "Tutoro - Tutorials for humans 🧐",
+    shortTitle: "Tutoro",
+    description: "Coding tutorials for humans 🧐",
+    keywords: "",
+    url: "",
+    prefix: "/",
+    language: "en_US",
+  },
+  manifest: {
+    name: "Tutoro",
+    shortName: "Tutoro",
+    startUrl: "/",
+    backgroundColor: "#101114",
+    themeColor: "#849DBB",
+    display: "standalone",
+  },
+}
