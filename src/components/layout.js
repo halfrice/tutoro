b5d1038561405b1ad169e1f9c23e7a44ab353afb
halfrice <halfrice@pm.me>
2020-02-27 21:41:58 -0800

    Customize webpack config for alias importing

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import "normalize.css/normalize.css"
-import GlobalStyles from "../styles/global"
+import GlobalStyles from "~styles/global"
 
 const Layout = ({ children }) => {
   return (
