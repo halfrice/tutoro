ee0921ffaad88a0ea5b927e120a017b63abc0da0
halfrice <halfrice@pm.me>
2020-03-04 00:05:41 -0800

    Expose home component to transition group

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
+import { CSSTransition, TransitionGroup } from "react-transition-group"
 import styled from "styled-components"
 import { Section, mixins, theme } from "~styles"
 
@@ -9,6 +10,11 @@ const { flex } = mixins
 const HomeContainer = styled(Section)`
   ${flex.start};
   flex-direction: column;
+`
+const TransitionContainer = styled(TransitionGroup)`
+  margin: 0 auto;
+  width: 100%;
+  max-width: 900px;
 `
 const Name = styled.h1`
   color: ${color.darkPink};
@@ -25,14 +31,44 @@ const Content = styled.div`
 `
 
 const Home = props => {
-  const { data } = props
-  const { frontmatter, html } = data[0].node
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setIsMounted(true), 200)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  const { frontmatter, html } = props.data[0].node
+
+  const name = () => (
+    <Name style={{ transitionDelay: "200ms" }}>{frontmatter.name}</Name>
+  )
+  const title = () => (
+    <Title style={{ transitionDelay: "400ms" }}>{frontmatter.title}</Title>
+  )
+  const content = () => (
+    <Content
+      style={{ transitionDelay: "600ms" }}
+      dangerouslySetInnerHTML={{ __html: html }}
+    >
+      {frontmatter.content}
+    </Content>
+  )
+
+  const items = [name, title, content]
 
   return (
     <HomeContainer>
-      <Name>{frontmatter.name}</Name>
-      <Title>{frontmatter.title}</Title>
-      <Content dangerouslySetInnerHTML={{ __html: html }} />
+      <TransitionContainer>
+        {isMounted &&
+          items.map((item, i) => {
+            return (
+              <CSSTransition key={i} classNames={`fadeup`} timeout={3000}>
+                {item}
+              </CSSTransition>
+            )
+          })}
+      </TransitionContainer>
     </HomeContainer>
   )
 }
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -44,7 +44,54 @@ const GlobalStyles = createGlobalStyle`
     font-size: ${fontSize.xs} !important;
   }
 
-  
+  .fadeup-enter {
+    opacity: 0.01;
+    transform: translateY(25px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .fadeup-enter-active {
+    opacity: 1;
+    transform: translateY(0px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .fadedown-enter {
+    opacity: 0.01;
+    transform: translateY(-25px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .fadedown-enter-active {
+    opacity: 1;
+    transform: translateY(0px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .fadeleft-enter {
+    opacity: 0.01;
+    transform: translateX(-50px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .fadeleft-enter-active {
+    opacity: 1;
+    transform: translateX(0px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .faderight-enter {
+    opacity: 0.01;
+    transform: translateX(50px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .faderight-enter-active {
+    opacity: 1;
+    transform: translateX(0px);
+    transition: opacity 500ms ${theme.easing}, transform 500ms ${theme.easing};
+  }
+  .fade-enter {
+    opacity: 0;
+    transition: opacity 1000ms ${theme.easing};
+  }
+  .fade-enter-active {
+    opacity: 1;
+    transition: opacity 1000ms ${theme.easing};
+  }
 `
 
 export default GlobalStyles
